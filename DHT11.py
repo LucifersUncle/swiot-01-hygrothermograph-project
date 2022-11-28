e149c7fcc7bb7017f096c11fa4b46e28c1ae7349
LucifersUncle <201801604@post.au.dk>
2022-11-28 13:19:51 +0100

    update RaspberryPi

--- DHT11.py
+++ DHT11.py
@@ -1,26 +1,44 @@
 import RPi.GPIO as GPIO
 import time
 import swiot_DHT as DHT
-import mqtt_client as mqtt
 import paho.mqtt.client as paho
 from paho import mqtt
 
 DHTPin = 11     #define the pin of DHT11
 
-client = mqtt.paho.Client(client_id="", userdata=None, protocol=mqtt.paho.MQTTv5)
-client.on_connect = mqtt.on_connect
+# setting callbacks for different events to see if it works, print the message etc.
+def on_connect(client, userdata, flags, rc, properties=None):
+    print("CONNACK received with code %s." % rc)
+
+# with this callback you can see if your publish was successful
+def on_publish(client, userdata, mid, properties=None):
+    print("mid: " + str(mid))
+
+# print which topic was subscribed to
+def on_subscribe(client, userdata, mid, granted_qos, properties=None):
+    print("Subscribed: " + str(mid) + " " + str(granted_qos))
+
+# print message, useful for checking if it was successful
+def on_message(client, userdata, msg):
+    print(msg.topic + " " + str(msg.qos) + " " + str(msg.payload))
+
+# using MQTT version 5 here, for 3.1.1: MQTTv311, 3.1: MQTTv31
+# userdata is user defined data of any type, updated by user_data_set()
+# client_id is the given name of the client
+client = paho.Client(client_id="", userdata=None, protocol=paho.MQTTv5)
+client.on_connect = on_connect
 
 # enable TLS for secure connection
-client.tls_set(tls_version=mqtt.mqtt.client.ssl.PROTOCOL_TLS)
+client.tls_set(tls_version=mqtt.client.ssl.PROTOCOL_TLS)
 # set username and password
 client.username_pw_set("swiot", "Mysecretpassword!")
 # connect to HiveMQ Cloud on port 8883 (default for MQTT)
 client.connect("71f8087751ae4fc6b20ce20b4820d6e9.s2.eu.hivemq.cloud", 8883)
 
 # setting callbacks, use separate functions like above for better visibility
-client.on_subscribe = mqtt.on_subscribe
-client.on_message = mqtt.on_message
-client.on_publish = mqtt.on_publish
+client.on_subscribe = on_subscribe
+client.on_message = on_message
+client.on_publish = on_publish
 
 
 def loop():
